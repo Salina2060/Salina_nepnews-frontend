a726c2d2ef715b3e0653431dc28bb3b80de02779
Salina2060 <166965942+Salina2060@users.noreply.github.com>
2025-05-06 09:50:27 +0545

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import NewsList from "../components/NewsList"; // Import NewsList
-import AdBanner from "../components/AdBanner"; // Import Ad Space Component
-import FeaturedNewsSlider from "../components/FeaturedNewsSlider"; // Import News Slider
-
-export default function Home() {
-  return (
-    <main className="min-h-screen" style={{ backgroundColor: '#050400' }}>
-      
-      {/* 🔥 Featured Slider Section (Top) */}
-      {/* <FeaturedNewsSlider /> */}
-
-      {/* 📢 Advertisement Banner Below Slider */}
-      <AdBanner type="home" />
-
-      {/* 📰 Latest News Section */}
-      <div className="container mx-auto">
-        <h1 className="text-3xl font-bold text-center my-6 text-white-600">
-          Latest News 📰
-        </h1>
-
-        {/* 🗞️ News Feed with Load More */}
-=======
 "use client";
 
 import NewsList from "../components/NewsList";
@@ -36,7 +13,6 @@ export default function Home() {
           Latest News 📰
         </h1>
 
->>>>>>> upstream/main
         <NewsList />
       </div>
     </main>
